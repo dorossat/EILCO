7e8f86753b5afac2f16586aba41d7b4758159ddc
Hamza HRAMCHI <hramchi.hamza@gmail.com>
2020-09-28 12:06:27 +0200

    Some manipulations

--- apprentissage-automatique/TP1/Script.R
+++ apprentissage-automatique/TP1/Script.R
@@ -23,16 +23,23 @@ c <- a+b
 print("c = a + b : ")
 print(c)
 c
-
 # --------------  Functions  -------------------------
 carre <- function(x) { return (x*x) }
 carre(3)
 
 # --------------   Vectors   -------------------------
+print('# --------------   Vectors   -------------------------')
 v1 <- vector("numeric", 10)
+print(v1)
 v2 <- vector("logical", 8)
+print(v2)
 v3 <- c(1,3,4,8)
+print(v3)
 v4 <- rep(1, 10)
+print(v4)
 v5 <- seq(1, 10)
+print(v5)
 v6 <- (1:10)
+print(v6)
 v7 <- seq(1, 10, 3)
+print(v7)
