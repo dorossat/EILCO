874e0d4234da1e7a61efd1d6782c63e58688c09e
Hamza HRAMCHI <hramchi.hamza@gmail.com>
2020-09-30 11:47:35 +0200

    Redirection through servlet

--- e-service/HelloWorldProject/src/main/java/h/hamza/servlets/HelloServlet.java
+++ e-service/HelloWorldProject/src/main/java/h/hamza/servlets/HelloServlet.java
@@ -8,21 +8,24 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet(name = "HelloServlet")
+// @WebServlet(name = "Hello")
 public class HelloServlet extends HttpServlet {
     public HelloServlet() {
         super();
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        //response.getWriter().append("Served at : ").append(request.getContextPath()).append(" (from doGet method)");
-        PrintWriter out = response.getWriter();
-        out.println("Hello from doGet method");
+        String nom = request.getParameter("nom");
+        request.setAttribute("name", "HRAMCHI");
+        request.getRequestDispatcher("Hello.jsp?name="+nom).forward(request, response); // can send attributes
+        // response.sendRedirect("Hello.jsp?name="+nom); //don't send attributes
+
+        // response.getWriter().append("Served at : ").append(request.getContextPath()).append(" (from doGet method)");
+        // PrintWriter out = response.getWriter();
+        // out.println("Hello from doGet method");
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         doGet(request,response);
     }
-
-
 }
---
+++ e-service/HelloWorldProject/src/main/webapp/Hello.jsp
@@ -0,0 +1,24 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Hamza HRAMCHI
+  Date: 30/09/2020
+  Time: 11:19
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1" pageEncoding="ISO-8859-1"%>
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+    <title>Insert title here</title>
+</head>
+<body>
+    Bonjour <%= request.getParameter("name") %> <br>
+    name :  <%=request.getAttribute("name") %>
+    <br/>
+    <%
+        String name = (String)request.getAttribute("name");
+        name = "M. " + name + " " +  request.getParameter("name");
+    %>
+    Hello <%= name %>
+</body>
+</html>
