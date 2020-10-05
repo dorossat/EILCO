81e9c3c9bc47f2eb0160db6c03ff315cd9b913e0
Hamza HRAMCHI <hramchi.hamza@gmail.com>
2020-10-05 15:52:25 +0200

    Second part

--- apprentissage-automatique/TP2/main.R
+++ apprentissage-automatique/TP2/main.R
@@ -6,7 +6,7 @@
 # ------ 1st part ------
 set.seed(0)
 generate <- function (n) {
-  bruit <- rnorm(15, 0.5);
+  bruit <- rnorm(n, 0.5);
   x <- runif(n, -10, 10)
   y <- (0.1*x^3 - 0.5*x^2 - x + 10) + bruit;
   return (matrix(c(x, y), nrow = n, ncol = 2))
@@ -15,6 +15,18 @@ generate(15)
 plot(generate(15), xlab = "X", ylab = "Y")
 
 # ------ 2nd part ------
-model1 <- lm(y ~ x)
+# --- Générer 5 modèles de régression avec M appartient à {1,2,3,6,12}
+
+model1 <- lm(y ~ poly(x, 1))
 print(model1)
-modelM <- lm(y ~ poly(x, M)) #  avec M appartient à {1,2,3,6,12}
+model2 <- lm(y ~ poly(x, 2))
+print(model2)
+model3 <- lm(y ~ poly(x, 3))
+print(model3)
+model6 <- lm(y ~ poly(x, 6))
+print(model6)
+model12 <- lm(y ~ poly(x, 12))
+print(model12)
+
+# --- Un vecteur z contenant une séquence de 300 valeursa allant de -10 à 10
+z <- seq(from = -10, to = 10, length.out = 300)
