85922124c95a4164c6f41d9388bc2540e66d5d2c
Hamza HRAMCHI <hramchi.hamza@gmail.com>
2020-10-07 09:57:17 +0200

    persistence

--- e-service/HelloWorldProject/src/main/java/h/hamza/model/HelloBean.java
+++ e-service/HelloWorldProject/src/main/java/h/hamza/model/HelloBean.java
@@ -1,11 +1,26 @@
 package h.hamza.model;
 
-import javax.servlet.http.HttpSession;
+import javax.persistence.*;
 import java.io.Serializable;
 
+@Entity
+@Table(schema="HelloDB", name="HelloTable")
+
 public class HelloBean implements Serializable {
+    private int id;
     private String name = "";
 
+    @Id
+    @GeneratedValue
+    @Column(name = "pk_hello")
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    @Column(name = "message")
     public String getName(){ return name; }
 
     public void setName(String name){ this.name = name; }
