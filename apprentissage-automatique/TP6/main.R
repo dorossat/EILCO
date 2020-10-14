b897c51c585c67d69106f956fd81b311328fa79b
Hamza HRAMCHI <hramchi.hamza@gmail.com>
2020-10-14 14:23:35 +0200

    TP6 : First exercice finish !

--- apprentissage-automatique/TP6/main.R
+++ apprentissage-automatique/TP6/main.R
@@ -8,7 +8,7 @@
 # -------------------------
 
 # -- La fonction kmeans()
-normalize = function(v){
+normalize <- function(v){
   max <- max(v)
   min <- min(v)
   v <- (v-min)/(max-min)
@@ -35,9 +35,30 @@ iris.3means$size
 plot (irisN$PetalLength, irisN$PetalWidth)
 
 # -- Colorer les points puis visualiser les groupes
-s_data1=subset(irisN,iris.3means$cluster==1)
+s_data1 <- subset(irisN,iris.3means$cluster==1)
 points(s_data1$PetalLength,s_data1$PetalWidth,col=2,pch=19)
-s_data2=subset(irisN,iris.3means$cluster==2)
+
+s_data2 <- subset(irisN,iris.3means$cluster==2)
 points(s_data2$PetalLength,s_data2$PetalWidth,col=3,pch=19)
-s_data3=subset(irisN,iris.3means$cluster==3)
+
+s_data3 <- subset(irisN,iris.3means$cluster==3)
 points(s_data3$PetalLength,s_data3$PetalWidth,col=4,pch=19)
+
+# -- Trouver le meilleur K
+avg_interclasse <- integer(9)
+avg_intraclasse <- integer(9)
+
+for(i in 2:10){
+  iris.kmeans <- kmeans(irisN[,1:4], centers=i, nstart=30) # Without Species
+  avg_interclasse[i-1] <- iris.kmeans$tot.withinss
+  avg_intraclasse[i-1] <- iris.kmeans$betweenss
+}
+
+dmatrix <- dist(irisN[,1:4])
+
+plot(2:10,avg_interclasse,type="b", main="Withinss en fonction de k", ylab="avg des sommes de withinss", xlab="K")
+plot(2:10,avg_intraclasse,type="b",main="Betweeness en fonction de k", ylab="avg des sommes de betweeness", xlab="K")
+
+
+
+
